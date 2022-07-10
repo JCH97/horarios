2e7d1853b0db514fd05c382e7eef2d7604ef3144
JCH97 <jc997hdez@gmail.com>
2022-07-10 00:18:00 -0400

    fix(semester)[feature/frontend]: backend - fix details.

---
+++ backend/src/semester/infra/entities/semester.persistence.ts
@@ -0,0 +1,36 @@
+import { PersistentEntity } from '../../../shared/modules/data-access/typeorm/base.entity';
+import { Column, Entity, Index, ManyToMany, OneToMany } from 'typeorm';
+import { FacultyPersistence } from '../../../faculty/infra/entities/faculty.persistence';
+import { LessonPersistence } from '../../../lesson/infra/entities/lesson.persistence';
+
+@Entity('semester')
+@Index(['id'], { unique: true })
+export class SemesterPersistence extends PersistentEntity {
+  @Column({ type: 'text' })
+  shortName: string;
+
+  @Column({ type: 'text' })
+  fullName: string;
+
+  @Column({ type: 'text' })
+  description: string;
+
+  @Column({ type: 'int' })
+  priority: number;
+
+  @Column({ type: 'float' })
+  duration: number;
+
+  @Column({ type: 'date' })
+  start: Date;
+
+  @Column({ type: 'date' })
+  end: Date;
+
+  @ManyToMany(
+    () => LessonPersistence,
+    lesson => lesson.semesters,
+    {},
+  )
+  lessons: LessonPersistence[];
+}
